8d8e4f668e8b4d2034cb0f4919343fe8c8ac3f31
Oleg Lavrovsky <oleg@utou.ch>
2017-12-06 12:24:00 +0100

    Descriptor Constraints model

--- src/descriptor.jl
+++ src/descriptor.jl
@@ -3,12 +3,34 @@ Table Schema descriptor
 https://github.com/frictionlessdata/tableschema-jl#descriptor
 """
 
+type Constraints
+    required::Bool
+    unique::Bool
+    # minLength:Integer
+    # maxLength:Ineger
+    # minimum
+    # maximum
+    # pattern
+    # enum
+
+    function Constraints(dict::Dict)
+        required = haskey(dict, "required") ?
+            dict["required"] : false
+        unique = haskey(dict, "unique") ?
+            dict["unique"] : false
+        new(required, unique)
+    end
+
+    function Constraints()
+        new(false, false)
+    end
+end
+
 type Descriptor
     _name::String
     _type::String
     _format::String
-    _required::Bool
-    _constraints::Dict
+    constraints::Constraints
 
     function Descriptor(dict::Dict)
         _name = dict["name"]
@@ -16,17 +38,15 @@ type Descriptor
             dict["type"] : DEFAULT_TYPE
         _format = haskey(dict, "format") ?
             dict["format"] : DEFAULT_FORMAT
-        _required = haskey(dict, "required") ?
-            dict["required"] : false
-        _constraints = Dict()
         if haskey(dict, "constraints")
-            _constraints["required"] = haskey(dict["constraints"], "required") ?
-                dict["constraints"]["required"] : false
+            constraints = Constraints(dict["constraints"])
+        else
+            constraints = Constraints()
         end
-        new(_name, _type, _format, _required, _constraints)
+        new(_name, _type, _format, constraints)
     end
 
     function Descriptor()
-        new("", "", "", false, Dict())
+        new("", "", "", Constraints())
     end
 end
